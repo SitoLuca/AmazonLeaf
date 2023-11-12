5f26d6338ca4e8ab5fc5aa0b8beb9591ccecf1db
viksn0w <vittoriopicone30@gmail.com>
2023-11-12 21:26:36 +0100

    Login funzionante 1. API funzionante con CORS abilitato 2. Creare funzione di Login e trovata metologia di usare SQLite (help) 3. Trovato modo per creare una sessione con javascript tramite cookie 4. Cambi estetici al login

--- JS/login.js
+++ JS/login.js
@@ -1,29 +1,27 @@
-function ValidateLogin() {
 
-    $.ajax({
+function validateLogin() {
+    const email = $('#email').val()
+    const password = $('#psw').val()
+    console.log(email, password)
+    const xhttpr = new XMLHttpRequest();
+    xhttpr.open('POST', 'http://127.0.0.1:8000/login', true);
 
-        url: "http://localhost:8080/login",
-        dataType: "json",
-        method: "post",
+    xhttpr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttpr.send(JSON.stringify({"email":email, "password":password}));
 
-        data: {
-
-            email: $("#email")[0].value,
-            psw: $("#psw")[0].value
-
-        },
-
-        success: function (res) {
-
-            console.log(res);
-
-        },
-        error: function (err) {
-            console.log(err)
+    xhttpr.onload = () => {
+        if (xhttpr.status === 200) {
+            const response = xhttpr.response
+            if(response !== "0"){
+                const parsed = JSON.parse(response)
+                console.log(parsed)
+                const obj = {"id":parsed[0],"surname":parsed[1], "name":parsed[2], "email":parsed[3]}
+                writeCookie("user", JSON.stringify(obj), 28)
+            } else {
+                console.log("Utente non trovato")
+            }
+        } else {
+            // Handle error
         }
-
-
-    })
-
-
+    };
 }
--- python/dbmanager.py
+++ python/dbmanager.py
@@ -1,18 +1,32 @@
 import sqlite3
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-api_port = 5000
+api_port = 8000 # sul Crispy McBacon la porta 5000 e 7000 sono esclusive :-(
 api_host = '127.0.0.1'
 app = Flask(__name__)
+CORS(app, resources={r'*':{'origins':'*'}})
 
-db = sqlite3.connect('../DB/amazonleafdb.sqlite')  # connect db
+
+db = sqlite3.connect('../DB/amazonleafdb.sqlite', check_same_thread=False)  # connect db
 cursor = db.cursor()
 
 @app.route('/login', methods=['POST'])
 def loginendpoint():
     data = request.get_json()
-    return jsonify(data)
+    sql = f"select * from operator o where o.email = '{data['email']}' and o.pass = '{data['password']}'"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        return "0"
+    else:
+        sending = jsonify(rows[0])
+        return sending
 
+@app.route('/test', methods=['GET'])
+def test():
+    return "Test"
 
 if __name__ == "__main__":
     app.run(debug=True, host=api_host, port=api_port)
+
